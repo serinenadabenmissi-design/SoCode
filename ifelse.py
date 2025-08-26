1d8e0a38a3d8501b50c35d2022b4ff2b94cbac37
serinenadabenmissi-design <serine_nada.benmissi@univ-constantine2.dz>
2025-08-27 00:00:37 +0200

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+age=input("enter your age: ")
+age=int(age)
+if age==18:
+    print("you can pass exam but with some conditions")
+elif age>18:
+        print("you can pass exam")
+else:
+      print("you can't pass exam")
