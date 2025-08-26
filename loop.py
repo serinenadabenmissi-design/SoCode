982a08c1a4ffaf24e8ed00a92b00ab33df98abea
serinenadabenmissi-design <serine_nada.benmissi@univ-constantine2.dz>
2025-08-27 00:06:26 +0200

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,9 @@
+for i in range(1,50,1):
+    if i%3==0 and i%5==0:
+        print(i,"fiss and base")
+    elif i%3==0:
+        print(i,"fiss")
+    elif i%5==0:
+        print(i,"base")    
+    else:
+        print (i)
