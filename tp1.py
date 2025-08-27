eb53a60187ae2c3e2712122cd63d0e9bb23ec785
serinenadabenmissi-design <serine_nada.benmissi@univ-constantine2.dz>
2025-08-28 00:12:21 +0200

    Add files via upload

---
+++ tp1.py
@@ -0,0 +1,7 @@
+last_name=input("enter your last name: ")
+first_name=input("enter your first name: ")
+
+if last_name!="" and first_name!="":
+    print(f"welcome {last_name} {first_name}")
+else:
+    print("Error")
